d9d32eb1ad10920e054ffcee1f6064022e2da0ea
Aashutosh <aashutoshsingh126@gmail.com>
2022-10-15 23:44:55 +0530

    initial setup commit

---
+++ restaurant-catalog-service/src/main/java/com/aashutosh/restaurantcatalogservice/RestaurantCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aashutosh.restaurantcatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantCatalogServiceApplication.class, args);
+    }
+
+}
